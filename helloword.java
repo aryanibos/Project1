c9f42c3b33182a349ed9da781279b80630570b2f
Arya Isnaidi <93307409+aryanibos@users.noreply.github.com>
2022-02-14 14:31:27 +0700

    Update helloword.java

--- helloword.java
+++ helloword.java
@@ -1,5 +1,8 @@
 public class HelloWord{
   public static vaoid main(){
     sistem.out.println("hllo dunia");
+    sistem.out.println("hllo dunia");
+    sistem.out.println("hllo dunia");
+    sistem.out.println("hllo dunia");
   }
 }
