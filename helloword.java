3f806ec4905872508ee63deadf772b3e6f4b859c
Arya Isnaidi <93307409+aryanibos@users.noreply.github.com>
2022-02-14 14:34:51 +0700

    nambah output globe,global,public

--- helloword.java
+++ helloword.java
@@ -1,8 +1,8 @@
 public class HelloWord{
   public static vaoid main(){
-    sistem.out.println("hllo dunia");
-    sistem.out.println("hllo globe");
-    sistem.out.println("hllo global");
-    sistem.out.println("hllo public");
+    sistem.out.println("hallo dunia");
+    sistem.out.println("hallo globe");
+    sistem.out.println("hallo global");
+    sistem.out.println("hallo public");
   }
 }
