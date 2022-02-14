2f4814ad13099e5b9d8083e5f5a082118e2ab713
Prana Yanuar Dana <pranadana55@gmail.com>
2022-02-14 14:35:22 +0700

    nambah output 12345

--- helloword.java
+++ helloword.java
@@ -1,5 +1,9 @@
 public class HelloWord{
   public static vaoid main(){
-    sistem.out.println("halo black furqon");
+    System.out.println("1");
+    System.out.println("2");
+    System.out.println("3");
+    System.out.println("4");
+    System.out.println("5");
   }
 }
