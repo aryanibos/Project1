0a154c5a833b13946a4e1ad66d9f0e5a092606c2
Arya Isnaidi <93307409+aryanibos@users.noreply.github.com>
2022-02-14 14:47:03 +0700

    Nambah nama Arya,Lutfi,Rahul

--- helloword.java
+++ helloword.java
@@ -1,8 +1,7 @@
 public class HelloWord{
   public static vaoid main(){
-    sistem.out.println("hallo dunia");
-    sistem.out.println("hallo globe");
-    sistem.out.println("hallo global");
-    sistem.out.println("hallo public");
+    sistem.out.println("Arya");
+    sistem.out.println("Luthfi");
+    sistem.out.println("Muzamil");
   }
 }
